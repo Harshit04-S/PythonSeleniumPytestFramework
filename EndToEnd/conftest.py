e93f1b62014494a112b73be8f05ab0b169b95195
harshit <harshit81451@gmail.com>
2026-04-15 09:45:33 +0530

    modified conftest filee

--- EndToEnd/conftest.py
+++ EndToEnd/conftest.py
@@ -34,30 +34,37 @@ def browserInstance(request):
 
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item):
-    """
-        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
-        :param item:
-        """
     pytest_html = item.config.pluginmanager.getplugin('html')
     outcome = yield
     report = outcome.get_result()
     extra = getattr(report, 'extra', [])
 
-    if report.when == 'call' or report.when == "setup":
+    if report.when in ("call", "setup"):
         xfail = hasattr(report, 'wasxfail')
+
         if (report.skipped and xfail) or (report.failed and not xfail):
-            reports_dir = os.path.join(os.path.dirname(__file__), 'Reports')
-            file_name = os.path.join(reports_dir, report.nodeid.replace("::", "_") + ".png")
-            print("file name is " + file_name)
-            _capture_screenshot(file_name)
-            if file_name:
-                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
-                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
-                extra.append(pytest_html.extras.html(html))
-        report.extras = extra
+
+            reports_dir = os.path.join(os.getcwd(), 'reports')
+            os.makedirs(reports_dir, exist_ok=True)
+
+            file_name = report.nodeid.replace("::", "_") + ".png"
+            file_path = os.path.join(reports_dir, file_name)
+
+            _capture_screenshot(file_path)
+
+            html = f'''
+            <div>
+                <img src="reports/{file_name}" alt="screenshot"
+                style="width:304px;height:228px;"
+                onclick="window.open(this.src)" align="right"/>
+            </div>
+            '''
+
+            extra.append(pytest_html.extras.html(html))
+
+    report.extras = extra
 
 
-def _capture_screenshot(file_name):
-    driver.get_screenshot_as_file(file_name)
-
+def _capture_screenshot(file_path):
+    driver.get_screenshot_as_file(file_path)
 
